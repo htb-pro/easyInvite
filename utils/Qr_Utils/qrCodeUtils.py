472e478cb86f8169f7bc664bb12b454ba4935db6
Ir heritier htb <101990754+htb-pro@users.noreply.github.com>
2026-02-01 13:22:46 +0100

    Update qrCodeUtils.py

--- utils/Qr_Utils/qrCodeUtils.py
+++ utils/Qr_Utils/qrCodeUtils.py
@@ -7,7 +7,7 @@ def createQrCode(token) -> str :
     qr_dirs = "static/Pictures/guestQrCode" #le chemin du repertoir ou seront stocke les images (qrCode)
     filename = f"{token}.png" #le nom du qrCode
     file_path = os.path.join(qr_dirs,filename)
-    url = f"http://localhost:8000/checkIn/{token}" #l'addresse ou sera stocker les qrcodes
+    url = f"http://easyinvite-1.onrender.com/{token}" #l'addresse ou sera stocker les qrcodes
     qr = QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -20,7 +20,7 @@ def createQrCode(token) -> str :
     return filename
 
 def createInviteQrCode(Itoken):
-    url = f"http://127.0.0.1:8000/{Itoken}"
+    url = f"http://easyinvite-1.onrender.com/{Itoken}"
     path = "static/Pictures/inviteQrCode"
     filename = f"{Itoken}.png" #Invite token 
     file_path = os.path.join(path,filename)
