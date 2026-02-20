a01a90f229ab6fad360198af1524e2793ee5b267
htb-pro <heritiertshim07@gmail.com>
2026-02-20 11:59:05 +0100

    add render-build lunix

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install -y libzbar0 libzbar-dev
+sudo apt-get install -y libzbar0 libzbar-dev 
 
 pip install --upgrade pip
-pip install -r requirement.txt
+pip install -r requirement.txt  
 
