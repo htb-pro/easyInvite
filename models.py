5bcc883126f0a286353cc4e5846b9ec8b4dbdc37
htb-pro <heritiertshim07@gmail.com>
2026-03-07 22:00:17 +0100

    last update 00

--- models.py
+++ models.py
@@ -43,7 +43,6 @@ class User(Base):
     password = Column(String,nullable = False)
     state = Column(String,default="active")
     created_at = Column(DateTime,default=datetime.utcnow())
-    group_id = Column(String,ForeignKey("groups.id"),nullable=True)
 
     #relationship
     groups = relationship("Group",secondary = user_groups,back_populates = "users")
