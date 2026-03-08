0d59570a12cea8c8e655557a0754d5642667050c
htb-pro <heritiertshim07@gmail.com>
2026-03-08 13:13:17 +0100

    update recreation.py

--- recreation.py
+++ recreation.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from db_setting import Base
 import os 
 
-DATABASE_URL = DB_URL =os.getenv('db_url_local')
+#DATABASE_URL = DB_URL =os.getenv('db_url_local')
+DATABASE_URL = DB_URL =os.getenv('db_url')
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
