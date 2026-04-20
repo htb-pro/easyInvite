fed3826d8251d46ea3aa6f3dfeb9bf43abacea8f
htb-pro <heritiertshim07@gmail.com>
2026-04-20 23:42:20 +0200

    add english template once

--- db_setting.py
+++ db_setting.py
@@ -6,9 +6,9 @@ import os,asyncio
 #---------------------initialising
 Base = declarative_base() #-----------------la classe mere pour la creation de table
 
-#load_dotenv()
+load_dotenv()
 DB_URL =os.getenv('db_url')
-DB_URL =os.getenv('db_url_local')
+#DB_URL =os.getenv('db_url_local')
 engine = create_async_engine(
     DB_URL,
     echo =True,
